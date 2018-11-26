385611c4b8667fa7c83f375693eb49b50d89b140
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-11-26 10:21:06 +0200

    Add Redux

---
+++ src/components/containers/AppContainer.js
@@ -0,0 +1,5 @@
+// @flow
+import App from '../App';
+import { withMatchParams } from '../../utils/container';
+
+export default withMatchParams(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+import 'react-app-polyfill/ie11';
+import '@babel/polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ThemeProvider } from 'styled-components';
+import theme from './config/theme';
+
+import * as serviceWorker from './serviceWorker';
+import configureStore from './config/configureStore';
+
+import App from './components/containers/AppContainer';
+
+const Root = () => (
+  <Provider store={configureStore()}>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Redirect exact path="/" to="/fi" />
+          <Route path="/:locale" component={App} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  </Provider>
+);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(<Root />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
