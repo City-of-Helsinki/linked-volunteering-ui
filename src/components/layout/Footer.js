4c56b4b98fde81da7b54c7ea02be47202d7c896a
Tommi <tommi.keskitalo@gmail.com>
2018-12-03 15:34:45 +0200

    Footer style improvements (#10)
    * WIP
    
    * remove eslint errros
    
    * add feedback
    
    * logo size
    
    * Update src/components/layout/Footer.js
    
    Co-Authored-By: Cifixie <tommi.keskitalo@gmail.com>
    
    * Update src/components/layout/Footer.js
    
    Co-Authored-By: Cifixie <tommi.keskitalo@gmail.com>
    
    * Update src/components/layout/Footer.js
    
    Co-Authored-By: Cifixie <tommi.keskitalo@gmail.com>

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,51 +2,60 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
-import { Col, Row, Container } from 'reactstrap';
 import Icon from '../common/Icon';
 
 const Wrapper = styled.div`
   background-color: ${props => props.theme.helCopper};
   color: #000;
   padding-top: 4em;
-  padding-bottom: 4em;
+  padding-left: 4em;
+  padding-right: 4em;
   min-height: 15em;
 
   & a {
     color: #000;
   }
 `;
+const NavigationLinks = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  span {
+    font-weight: 600;
+  }
+  span + span {
+    margin-left: 3em;
+  }
+`;
+const LogoWrapper = styled.div`
+  border-bottom: 1px solid ${props => props.theme.helWhite};
+`;
 
-const FooterSection = styled(Col)`
-  text-align: center;
+const BottonLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1em 0;
 `;
 
 const Footer = () => (
   <Wrapper>
-    <Container fluid>
-      <Row>
-        <FooterSection md="6">
-          <Icon name="helsinkiLogo" width="120px" color="#000" />
-          <div>&copy; Copyright 2018 - Kaikki oikeudet pidätetään.</div>
-        </FooterSection>
-        <FooterSection md="6">
-          <div>
-            <a href="#">
-              <FormattedMessage tagName="span" id="site.footer.front_page" />
-            </a>
-            <a href="#">
-              <FormattedMessage tagName="span" id="site.footer.upcoming_events" />
-            </a>
-            <a href="#">
-              <FormattedMessage tagName="span" id="site.footer.create_event" />
-            </a>
-            <a href="#">
-              <FormattedMessage tagName="span" id="site.footer.contacts" />
-            </a>
-          </div>
-        </FooterSection>
-      </Row>
-    </Container>
+    <NavigationLinks>
+      <FormattedMessage tagName="span" id="site.footer.front_page" />
+      <FormattedMessage tagName="span" id="site.footer.upcoming_events" />
+      <FormattedMessage tagName="span" id="site.footer.create_event" />
+      <FormattedMessage tagName="span" id="site.footer.contacts" />
+    </NavigationLinks>
+    <LogoWrapper>
+      <Icon name="helsinkiLogo" width="150px" height="100px" color="#000" />
+    </LogoWrapper>
+    <BottomLinks>
+      <div>
+        <span>&copy; </span>
+        <FormattedMessage tagName="span" id="site.footer.copyright" />
+      </div>
+      <div>
+        <FormattedMessage tagName="span" id="site.footer.give_feedback" />
+      </div>
+    </BottomLinks>
   </Wrapper>
 );
 
