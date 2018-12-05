1b52049b62c99ac0df6a7319338e39d54c729df0
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-12-05 15:05:37 +0200

    Add dummy data

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,8 +1,10 @@
 // @flow
 import type { Event } from '../types/event';
+import eventsData from '../../api/events.json';
 
 export default {
   submit: async (event: Event): Promise<void> => {
     console.debug(event);
-  }
+  },
+  getEvents: async (): Promise<Event> => eventsData
 };
