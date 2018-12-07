e93e67fd3d0463e1f7e82f68138833055164960f
Tommi <mato12@hotmail.fi>
2018-12-07 08:07:09 +0200

    Dirty is a boolean

--- src/types/forms.js
+++ src/types/forms.js
@@ -16,7 +16,7 @@ export type WithForm<T> = {
   values: T,
   errors: $ObjMap<$Shape<T>, (mixed) => string>,
   touched: $ObjMap<$Shape<T>, (mixed) => boolean>,
-  dirty: $ObjMap<$Shape<T>, (mixed) => boolean>,
+  dirty: boolean,
   handleChange?: handleEvent<mixed>,
   handleBlur?: handleEvent<mixed>,
   handleSubmit?: handleEvent<mixed, HTMLButtonElement>,
