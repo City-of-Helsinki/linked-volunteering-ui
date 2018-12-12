ffb84b3118c58551c90e1bbb49b426fd312125d4
Tommi <mato12@hotmail.fi>
2018-12-12 18:46:18 +0200

    Best I can do for now to fix background image

--- src/components/pages/events/SubmittedPage.js
+++ src/components/pages/events/SubmittedPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 import IntlComponent from '../../common/IntlComponent';
 import LocalizedLink from '../../common/LocalizedLink';
@@ -13,62 +13,64 @@ import backgroundImage from '../../../assets/images/_MG_2851_c_Jussi_Hellsten.jp
 
 const PageContainer = styled.div`
   background-image: url(${backgroundImage});
-  background-size: 40% 100%;
+  background-size: 45% 100%;
   background-repeat: no-repeat;
   background-position: right;
-  padding-top: 12vh;
-  padding-bottom: 30vh;
-  h1 {
-    font-size: 3.5rem;
-    line-height: 0.9;
-  }
-  p,
-  a,
-  strong {
-    display: block;
-    font-size: 1.25em;
-    margin-top: 1.5rem;
-    margin-bottom: 2.5rem;
-  }
 `;
+
 const ButtonWrapper = styled.div`
   display: flex;
   align-items: center;
   color: black;
 `;
 
+const Content = styled.div`
+  width: 55%;
+  padding-top: 12vh;
+  padding-bottom: 30vh;
+  padding-left: 4vw;
+  padding-right: 6vw;
+
+  h1 {
+    font-size: 3.5rem;
+    line-height: 0.9;
+  }
+
+  p,
+  strong {
+    display: block;
+  }
+
+  p,
+  a,
+  strong {
+    font-size: 1.25em;
+    margin-top: 1.5rem;
+    margin-bottom: 2.5rem;
+  }
+`;
+
 const SubmittedPage = () => (
   <Layout>
     <PageContainer>
-      <Container>
-        <Row>
-          <Col md="6">
-            <FormattedMessage tagName="h1" id="site.page.thank_you.header" />
-            <FormattedMessage tagName="strong" id="site.page.thank_you.paragraph" />
-          </Col>
-        </Row>
-        <Row>
-          <Col md="6">
-            <ButtonWrapper>
-              <IntlComponent
-                Component={Button}
-                color="link"
-                id="site.page.thank_you.action.add_to_calendar"
-              />
-            </ButtonWrapper>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="6">
-            <IntlComponent
-              Component={LocalizedLink}
-              className="btn btn-primary"
-              id="site.page.thank_you.action.home_page"
-              to=""
-            />
-          </Col>
-        </Row>
-      </Container>
+      <Content>
+        <FormattedMessage tagName="h1" id="site.page.thank_you.header" />
+        <FormattedMessage tagName="strong" id="site.page.thank_you.paragraph" />
+
+        <ButtonWrapper>
+          <IntlComponent
+            Component={Button}
+            color="link"
+            id="site.page.thank_you.action.add_to_calendar"
+          />
+        </ButtonWrapper>
+        <IntlComponent
+          Component={LocalizedLink}
+          className="btn btn-primary"
+          id="site.page.thank_you.action.home_page"
+          to=""
+        />
+      </Content>
     </PageContainer>
   </Layout>
 );
