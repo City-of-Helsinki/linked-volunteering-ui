52b6ff1522e9438dbb6783c872e15f0c7aacc1aa
Tommi <mato12@hotmail.fi>
2018-12-13 10:10:50 +0200

    WIP

--- src/components/pages/events/EventsPage.js
+++ src/components/pages/events/EventsPage.js
@@ -1,8 +1,21 @@
+// @flow
+
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 import LocalizedLink from '../../common/LocalizedLink';
 import Layout from '../../layout/Layout';
 
-class EventsPage extends PureComponent {
+import type { Events } from '../../../types/event';
+
+type Props = {
+  events: Events
+};
+
+const Wrapper = styled.div`
+  margin-bottom: 3em;
+`;
+
+class EventsPage extends PureComponent<Props> {
   componentDidMount() {
     const { events, getEvents } = this.props;
     if (!events) {
@@ -14,14 +27,22 @@ class EventsPage extends PureComponent {
     const { events } = this.props;
     return (
       <Layout>
-        <ul>
-          {events &&
-            events.results.valueSeq().map(event => (
-              <li key={event.id}>
-                <LocalizedLink to={`event/modify/${event.id}`}>{event.name}</LocalizedLink>
-              </li>
-            ))}
-        </ul>
+        <Wrapper>
+          {events && (
+            <table>
+              <tbody>
+                {events.results.valueSeq().map(event => (
+                  <tr key={event.id}>
+                    <td>{event.name}</td>
+                    <td>
+                      <LocalizedLink to={`event/modify/${event.id}`}>{event.name}</LocalizedLink>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </Wrapper>
       </Layout>
     );
   }
