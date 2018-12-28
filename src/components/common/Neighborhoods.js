c20b05fe5f002e8c32fe8b45bda1ff6ee44cff6a
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-12-28 08:11:31 +0200

    Replaced select with Select component

--- src/components/common/Neighborhoods.js
+++ src/components/common/Neighborhoods.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Select from '../form/fields/Select';
 
 const Neighborhoods = ({ neighborhoods, onChange }) => (
-  <select onChange={onChange}>
-    <option value="" />
+  <Select onChange={onChange}>
     {neighborhoods.valueSeq().map(neighborhood => (
       <option key={neighborhood.ocd_id} value={neighborhood.ocd_id}>
         {neighborhood.name.fi}
       </option>
     ))}
-  </select>
+  </Select>
 );
 
 export default Neighborhoods;
