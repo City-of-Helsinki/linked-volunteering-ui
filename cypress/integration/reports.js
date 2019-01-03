868a270b7b11c2fe7005626cce79f7261f9cfdea
Artur Gajewski <artur.gajewski@fraktio.fi>
2019-01-03 15:03:15 +0200

    Tests for selecting yearly reports

---
+++ cypress/integration/reports.js
@@ -0,0 +1,20 @@
+describe('Yearly reports', () => {
+  it('Initially there are no reports', () => {
+    cy.visit('/fi/report');
+    cy.contains('Talkoita yhteensä 0');
+    cy.contains('Osallistujia yhteensä 0');
+  });
+
+  it('Loads reports from selected years', () => {
+    cy.visit('/fi/report');
+    cy.get('#area').select('2019');
+    cy.contains('Talkoita yhteensä 10');
+    cy.contains('Osallistujia yhteensä 26');
+    cy.get('#area').select('2018');
+    cy.contains('Talkoita yhteensä 43');
+    cy.contains('Osallistujia yhteensä 123');
+    cy.get('#area').select('2017');
+    cy.contains('Talkoita yhteensä 20');
+    cy.contains('Osallistujia yhteensä 45');
+  });
+});
