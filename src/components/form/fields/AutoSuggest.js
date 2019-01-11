9101a6ed1ab45a00e468869155111760b6f83e3c
Tommi <tommi.keskitalo@gmail.com>
2019-01-11 12:27:55 +0200

    React autosuggest (#61)
    * MWP
    
    * Styles
    
    * Style improvements
    
    * use getSuggestionValue with renderSuggestion
    
    * Fix tests

---
+++ src/components/form/fields/AutoSuggest.js
@@ -0,0 +1,109 @@
+import React, { PureComponent } from 'react';
+import Autosuggest from 'react-autosuggest';
+import { FormGroup, Input, FormFeedback, FormText } from 'reactstrap';
+import styled from 'styled-components';
+import { injectIntl } from 'react-intl';
+import Label from './Label';
+import './AutoSuggest.scss';
+
+const StyledFormGroup = styled(FormGroup)`
+  margin-bottom: 0;
+`;
+
+const simplify = value => value.toLowerCase();
+
+class Example extends PureComponent {
+  state = {
+    value: '',
+    suggestions: []
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState(() => ({
+      value: newValue
+    }));
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    const { suggestions, getSuggestionValue } = this.props;
+    this.setState(() => ({
+      suggestions: suggestions.filter(suggestion =>
+        simplify(getSuggestionValue(suggestion)).includes(simplify(value))
+      )
+    }));
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState(() => ({
+      suggestions: []
+    }));
+  };
+
+  onSuggestionSelected = (event, { suggestion }) => {
+    const { id, onChange } = this.props;
+    onChange({
+      target: {
+        id,
+        value: suggestion
+      }
+    });
+  };
+
+  renderSuggestion = suggestion => {
+    const { getSuggestionValue } = this.props;
+    return <span>{getSuggestionValue(suggestion)}</span>;
+  };
+
+  render() {
+    const {
+      id,
+      label,
+      required,
+      error,
+      touched,
+      text,
+      placeholder,
+      intl: { formatMessage },
+      onBlur,
+      getSuggestionValue
+    } = this.props;
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      value,
+      onBlur,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        onSuggestionSelected={this.onSuggestionSelected}
+        renderSuggestion={this.renderSuggestion}
+        getSuggestionValue={getSuggestionValue}
+        inputProps={inputProps}
+        renderInputComponent={props => (
+          <StyledFormGroup>
+            {label && (
+              <Label htmlFor={id} required={required}>
+                {formatMessage({ id: label })}
+              </Label>
+            )}
+            <Input
+              id={id}
+              invalid={error && touched}
+              placeholder={placeholder ? formatMessage({ id: placeholder }) : undefined}
+              {...props}
+            />
+            <FormFeedback>{error && formatMessage({ id: error })}</FormFeedback>
+            <FormText>{text && formatMessage({ id: text })}</FormText>
+          </StyledFormGroup>
+        )}
+      />
+    );
+  }
+}
+
+export default injectIntl(Example);
