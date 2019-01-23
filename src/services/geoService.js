e0c03b26af71e2b7fdfabf29bd481bbb60d0d55a
Artur Gajewski <artur.gajewski@fraktio.fi>
2019-01-23 07:58:58 +0200

    Fix incorrect params for geo

--- src/services/geoService.js
+++ src/services/geoService.js
@@ -2,7 +2,7 @@ import { get } from '../utils/api';
 
 export default {
   getAddress: async (lat, lon) => {
-    const payload = await get(`geo_query/?lat=${lat},lon=${lon}`);
+    const payload = await get(`geo_query/?lat=${lat}&lon=${lon}`);
     return payload.closest_address;
   }
 };
