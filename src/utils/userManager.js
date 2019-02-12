5c86b4996179257a86b3795835ee3788528b60f3
Artur Gajewski <artur.gajewski@fraktio.fi>
2019-02-12 16:26:19 +0200

    Chnages

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -1,6 +1,10 @@
 import { createUserManager } from 'redux-oidc';
 
-const { REACT_APP_SSO_URL, REACT_APP_OPENID_CLIENT_ID } = process.env;
+const {
+  REACT_APP_SSO_URL,
+  REACT_APP_OPEN_ID_API_TOKENS_SCOPE,
+  REACT_APP_OPENID_CLIENT_ID
+} = process.env;
 
 const getRedirectUri = ext => {
   const { protocol, hostname, port } = window.location;
@@ -14,7 +18,7 @@ const settings = {
   post_logout_redirect_uri: getRedirectUri('logged_out'),
   loadUserInfo: true,
   response_type: 'id_token token',
-  scope: 'openid profile https://api.hel.fi/auth/haravadev'
+  scope: `openid profile ${REACT_APP_OPEN_ID_API_TOKENS_SCOPE}`
 };
 
 export default createUserManager(settings);
