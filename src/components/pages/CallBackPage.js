51b1097fc503530a36d9206f0b488aa4d75548e9
Artur Gajewski <info@arturgajewski.com>
2019-02-13 15:23:39 +0200

    Fetch api access token on page refresh (#70)

--- src/components/pages/CallBackPage.js
+++ src/components/pages/CallBackPage.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { CallbackComponent } from 'redux-oidc';
-import axios from 'axios';
 import userManager from '../../utils/userManager';
 
-const { REACT_APP_SSO_URL, REACT_APP_OPEN_ID_API_TOKENS_SCOPE } = process.env;
-
-const CallBackPage = ({ history, setApiAccessToken }) => (
+const CallBackPage = ({ history, getApiAccessToken }) => (
   <CallbackComponent
     userManager={userManager}
     successCallback={async () => {
-      const user = await userManager.getUser();
-      const accessToken = user.access_token;
-      const userAuth = await axios.get(`${REACT_APP_SSO_URL}/api-tokens/`, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-        scope: REACT_APP_OPEN_ID_API_TOKENS_SCOPE
-      });
-
-      setApiAccessToken(userAuth.data[REACT_APP_OPEN_ID_API_TOKENS_SCOPE]);
-
+      getApiAccessToken();
       history.push('/');
     }}
     errorCallback={error => {
--- src/components/pages/containers/CallBackPageContainer.js
+++ src/components/pages/containers/CallBackPageContainer.js
@@ -1,13 +1,13 @@
 import { compose } from 'recompose';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
-import { setApiAccessToken } from '../../../ducks/auth';
+import { getApiAccessToken } from '../../../ducks/auth';
 import CallBackPage from '../CallBackPage';
 
 export default compose(
   withRouter,
   connect(
     null,
-    { setApiAccessToken }
+    { getApiAccessToken }
   )
 )(CallBackPage);
