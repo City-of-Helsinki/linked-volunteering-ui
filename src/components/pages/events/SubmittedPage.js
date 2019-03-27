a44babac1672484df956bfa762cfc0a3e143fbc0
Artur Gajewski <artur.gajewski@fraktio.fi>
2019-03-27 14:45:08 +0200

    Changed breakboint

--- src/components/pages/events/SubmittedPage.js
+++ src/components/pages/events/SubmittedPage.js
@@ -17,17 +17,28 @@ const PageContainer = styled.div`
 
 const Content = styled.div`
   flex: 0 0 55%;
-  padding-left: 7em;
-  padding-right: 7em;
-  padding-top: 3.5em;
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: 2em;
   padding-bottom: 3.5em;
   display: flex;
   flex-direction: column;
   justify-content: center;
 
+  ${responsive.sm`
+    padding-left: 7em;
+    padding-right: 7em;
+    padding-top: 3.5em;
+
+    h1 {
+      font-size: 4rem;
+      line-height: 0.9;
+    }
+  `}
+
   h1 {
-    font-size: 3.5rem;
-    line-height: 0.9;
+    font-size: 2.5rem;
+    line-height: 1;
   }
 
   p,
