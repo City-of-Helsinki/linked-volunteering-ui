4913be0c713cf89f9470edc3f36da270f762b4df
Jori Lindell <jori.lindell@gmail.com>
2019-11-19 13:59:51 +0200

    Change "placeholder" date to datepickers

--- src/components/form/partitions/DateRange.js
+++ src/components/form/partitions/DateRange.js
@@ -1,6 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Row, Col } from 'reactstrap';
-import { addDays } from 'date-fns';
+import { addDays, addHours } from 'date-fns';
 import { injectIntl } from 'react-intl';
 
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
@@ -30,6 +30,10 @@ class DateTime extends PureComponent {
     });
   };
 
+  handleDateChange = (id, oldDate) => value => {
+    this.onChange(id)(oldDate ? value : addHours(value, 9));
+  };
+
   onBlur = id => value => {
     const { handleBlur } = this.props;
     handleBlur({
@@ -38,6 +42,17 @@ class DateTime extends PureComponent {
         value
       }
     });
+  };
+
+  getDateFormat = locale => {
+    switch (locale) {
+      case 'en':
+        return 'dd/MM/yyyy';
+      case 'sv':
+      case 'fi':
+      default:
+        return 'dd.MM.yyyy';
+    }
   };
 
   getTimeFormat = locale => {
@@ -62,6 +77,7 @@ class DateTime extends PureComponent {
     } = this.props;
     const selectedStartTime = values.start_time;
     const selectedEndTime = values.end_time;
+    const dateFormat = this.getDateFormat(locale);
     const timeFormat = this.getTimeFormat(locale);
 
     return (
@@ -77,10 +93,11 @@ class DateTime extends PureComponent {
               locale={locale}
               error={errors.start_time}
               touched={touched.start_time}
-              onChange={this.onChange('start_time')}
+              onChange={this.handleDateChange('start_time', values.start_time)}
               onBlur={this.onBlur('start_time')}
               highlightDates={[now]}
               selected={selectedStartTime}
+              dateFormat={dateFormat}
               minDate={minDate}
               maxDate={values.end_time}
               startDate={values.start_time}
@@ -100,9 +117,10 @@ class DateTime extends PureComponent {
               locale={locale}
               error={errors.end_time}
               touched={touched.end_time}
-              onChange={this.onChange('end_time')}
+              onChange={this.handleDateChange('end_time', values.end_time)}
               onBlur={this.onBlur('end_time')}
               selected={selectedEndTime}
+              dateFormat={dateFormat}
               minDate={values.start_time || minDate}
               startDate={values.start_time}
               endDate={values.end_time}
