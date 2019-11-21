13f3e1a99c99850c5b4dd5b110406206c50da7d0
Jori Lindell <jori.lindell@gmail.com>
2019-11-21 21:25:24 +0200

    Set fi as default language if language is not selected

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,27 @@ import { Switch, Route } from 'react-router';
 import { IntlProvider } from 'react-intl';
 
 import messages from '../config/translations';
-
+import { DEFAULT_LANGUAGE, SUPPORT_LANGUAGES } from '../constants';
 import NewEventPage from './pages/events/containers/NewEventPageContainer';
 import SubmittedPage from './pages/events/containers/SubmittedPageContainer';
 import LandingPage from './pages/LandingPage';
 import AdminRoutes from './Admin';
 import Login from './Login';
 
-const App = ({ locale }) => (
-  <IntlProvider locale={locale} key={locale} messages={messages[locale]}>
-    <Switch>
-      <Route exact path="/login/" component={Login} />
-      <Route exact path="/:locale/" component={LandingPage} />
-      <Route exact path={`/:locale/event/new`} component={NewEventPage} />
-      <Route exact path={`/:locale/event/submitted`} component={SubmittedPage} />
-      <Route path={`/:locale/admin`} component={AdminRoutes} />
-    </Switch>
-  </IntlProvider>
-);
+const App = ({ locale }) => {
+  const language = SUPPORT_LANGUAGES[locale.toUpperCase()] || DEFAULT_LANGUAGE;
+
+  return (
+    <IntlProvider locale={language} key={language} messages={messages[language]}>
+      <Switch>
+        <Route exact path="/login/" component={Login} />
+        <Route exact path="/:locale/" component={LandingPage} />
+        <Route exact path={`/:locale/event/new`} component={NewEventPage} />
+        <Route exact path={`/:locale/event/submitted`} component={SubmittedPage} />
+        <Route path={`/:locale/admin`} component={AdminRoutes} />
+      </Switch>
+    </IntlProvider>
+  );
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SUPPORT_LANGUAGES = {
+  EN: 'en',
+  FI: 'fi',
+  SV: 'sv'
+};
+
+export const DEFAULT_LANGUAGE = 'fi';
