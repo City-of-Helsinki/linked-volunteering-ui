1308f337d680e2b55e80bdd1e29c26584ec53d12
Jori Lindell <jori.lindell@gmail.com>
2020-03-06 09:37:33 +0200

    Fix routing

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,10 @@ import { IntlProvider } from 'react-intl';
 
 import messages from '../config/translations';
 import { DEFAULT_LANGUAGE, SUPPORT_LANGUAGES } from '../constants';
-import AccessibilityPage from './pages/accessibility/AccessibilityPage.tsx';
-import NewEventPage from './pages/events/containers/NewEventPageContainer';
-import SubmittedPage from './pages/events/containers/SubmittedPageContainer';
-import LandingPage from './pages/LandingPage.tsx';
 import Error404Page from './pages/Error404Page.tsx';
+import LandingPage from './pages/LandingPage.tsx';
 import AdminRoutes from './Admin';
+import LocaleRoutes from './LocaleRoutes.tsx';
 import Login from './Login';
 
 const App = ({ locale }) => {
@@ -22,11 +20,9 @@ const App = ({ locale }) => {
           <Redirect to="fi" />
         </Route>
         <Route exact path="/login/" component={Login} />
-        <Route exact path={`/${language}`} component={LandingPage} />
-        <Route path={`/${language}/accessibility`} component={AccessibilityPage} />
-        <Route exact path={`/${language}/event/new`} component={NewEventPage} />
-        <Route exact path={`/${language}/event/submitted`} component={SubmittedPage} />
         <Route path={`/${language}/admin`} component={AdminRoutes} />
+        <Route exact path={`/${language}`} component={LandingPage} />
+        <Route path={`/${language}/*`} component={LocaleRoutes} />
         <Route path="*" component={Error404Page} />
       </Switch>
     </IntlProvider>
