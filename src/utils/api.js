48d3c1874f7e1621f00706605b21837a077a769d
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-03 11:27:13 +0300

    Capture API errors with Sentry
    ref: PUIS-74

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as Sentry from '@sentry/browser';
 
 const { REACT_APP_API_URL } = process.env;
 
@@ -6,6 +7,18 @@ const instance = axios.create({
   baseURL: `${REACT_APP_API_URL || '/'}/v1/`,
   timeout: 3000
 });
+
+// Capture API errors into Sentry to make debugging easier.
+const errorInterceptionArguments = [
+  config => config,
+  error => {
+    Sentry.captureException(error);
+
+    return Promise.reject(error);
+  }
+];
+instance.interceptors.request.use(...errorInterceptionArguments);
+instance.interceptors.response.use(...errorInterceptionArguments);
 
 const securityHeader = apiAccessToken =>
   apiAccessToken ? { Authorization: `Bearer ${apiAccessToken}` } : null;
