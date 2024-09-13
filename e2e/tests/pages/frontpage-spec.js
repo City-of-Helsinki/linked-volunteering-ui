0f24b2e746527968d58ecc2a4a344ed715a7922e
Tero Virtanen <tero.virtanen@mavericks.fi>
2024-09-13 13:31:16 +0300

    build: initial playwright e2e test

---
+++ e2e/tests/pages/frontpage-spec.js
@@ -0,0 +1,21 @@
+import { Page, expect, test } from '@playwright/test';
+
+
+test.describe("Frontpage", () => {
+  let page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+
+    await page.goto('/');
+  });
+
+  test('title', async () => {
+    const pageTitle = await page.title();
+    expect(pageTitle).toContain("Puistotalkoot");
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+})
