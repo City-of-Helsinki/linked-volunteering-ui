eb67083abd688d05228948f0b7abbaa54e4d2ee1
mikkojamG <mikko.jamia@gofore.com>
2025-05-21 14:04:54 +0300

    deps: remove unnecessary packages PS-189

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import './polyfills';
 import 'bootstrap/dist/css/bootstrap.css';
 import './config/theme.scss';
 
--- src/polyfills.js
+++
@@ -1,6 +0,0 @@
-if (typeof window.TextEncoder !== 'function') {
-  // eslint-disable-next-line global-require
-  const TextEncodingPolyfill = require('text-encoding');
-  window.TextEncoder = TextEncodingPolyfill.TextEncoder;
-  window.TextDecoder = TextEncodingPolyfill.TextDecoder;
-}
