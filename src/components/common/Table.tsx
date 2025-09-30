957943187b7890982d7b5a559bbf374b12bdc051
Joonas Mykkänen <42795051+JoonasMykkanen@users.noreply.github.com>
2025-09-30 08:05:01 +0300

    feat: force a scrollbar to be present (#265)

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -69,8 +69,19 @@ const StyledFirstTd = styled.td<{ highlighted?: boolean }>`
 `;
 
 const StyledTableWrapper = styled.div`
-  overflow: auto;
+  overflow-x: scroll;
   max-width: calc(100vw - 2rem);
+
+  ::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 7px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+  }
 `;
 
 const StyledTable = styled.table<{ firstColumn?: boolean }>`
