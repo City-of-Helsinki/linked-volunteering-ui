2025c38621ebac9909b341b06076f841ae35c5c3
mikkojamG <mikko.jamia@gofore.com>
2026-05-18 14:20:13 +0300

    ci: dynamic runtime envs Refs: PS-276

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,10 @@
 /* eslint-disable no-console */
 import '@testing-library/jest-dom/vitest';
 
+// Load generated runtime configuration to be available in tests
+// eslint-disable-next-line @typescript-eslint/no-require-imports, import-x/extensions
+require('../public/test-env-config.js');
+
 window.scrollTo = vi.fn();
 
 const originalError = console.error.bind(console.error);
