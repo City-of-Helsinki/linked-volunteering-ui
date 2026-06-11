76244c2a38a909fc3e332d6a5e7df421b118cc3b
Toivo Mattila <toivo.mattila@virnex.fi>
2026-06-11 12:44:55 +0300

    refactor(test): remove unused screen import
    Refs: PS-275

--- src/components/form/partitions/__tests__/Location.test.tsx
+++ src/components/form/partitions/__tests__/Location.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import { vi } from 'vitest';
 
